81df9b19ca910d082668856f7c01ebd39397f8a9
Zeeshan Khan <zeeskhan1990@gmail.com>
2018-01-31 18:46:31 +0530

    Add header command bar + callout

---
+++ app/globals/header.ts
@@ -0,0 +1,55 @@
+export const items = [
+    {
+      key: 'headerMenu',
+      name: '',
+      icon: 'CollapseMenu',
+      ariaLabel: 'Main menu of Coglite',
+      ['data-automation-id']: 'newHeaderMenu',
+      onClick: () => { return; }
+    },
+    {
+      key: 'coglite',
+      name: 'Coglite',
+      ariaLabel: 'Coglite Header',
+      ['data-automation-id']: 'uploadButton',
+      onClick: () => { return; }
+    }
+  ];
+  
+  export const farItems = [
+    {
+      key: 'userConfig',
+      name: 'User Name - Coglite Workspace',
+      onClick: () => { return; }
+    },
+    {
+      key: 'help',
+      name: '',
+      icon: 'FeedbackRequestSolid',
+      onClick: () => { return; }
+    },
+    {
+      key: 'chat',
+      name: '',
+      icon: 'ChatSolid',
+      onClick: () => { return; }
+    },
+    {
+      key: 'forum',
+      name: '',
+      icon: 'Group',
+      onClick: () => { return; }
+    },
+    {
+      key: 'submitFeedback',
+      name: '',
+      icon: 'Emoji2',
+      onClick: () => { return; }
+    },
+    {
+      key: 'accountManagement',
+      name: '',
+      icon: 'AccountManagement',
+      onClick: () => { return; }
+    }
+  ];
